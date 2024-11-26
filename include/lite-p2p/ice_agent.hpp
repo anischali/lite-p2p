77c51e6cf63299e465966d703eb54e4edbebf5f3
Anis CHALI <anis.chali1@outlook.com>
2024-11-25 21:03:49 -0500

    ice_agent: add an ice agent

--- include/lite-p2p/ice_agent.hpp
+++ include/lite-p2p/ice_agent.hpp
@@ -1,5 +1,7 @@
 #ifndef __ICE_AGENT_HPP__
 #define __ICE_AGENT_HPP__
+#include <vector>
+#include <lite-p2p/network.hpp>
 
 namespace lite_p2p
 {
@@ -7,10 +9,16 @@ namespace lite_p2p
     class ice_agent
     {
     private:
+        std::vector<struct sockaddr_t> addrs;
+        std::vector<lite_p2p::network> ifaces_info;
+
         /* data */
     public:
         ice_agent();
         ~ice_agent();
+
+        void gather_addrs(void);
+        std::vector<struct sockaddr_t> get_addrs(void) {return addrs;};
     };
 };
 #endif
--- lib/ice_agent.cpp
+++ lib/ice_agent.cpp
@@ -1,12 +1,27 @@
-#include "ice_agent.hpp"
+#include "lite-p2p/ice_agent.hpp"
 
 using namespace lite_p2p;
 
-    ice_agent::ice_agent()
-    {
-    }
+ice_agent::ice_agent()
+{
+}
 
-    ice_agent::~ice_agent()
-    {
-    }
+ice_agent::~ice_agent()
+{
+}
 
+
+void ice_agent::gather_addrs(void) {
+    std::vector<std::string> ifaces = network::network_interfaces();
+
+    for (auto &&ifc : ifaces) {
+        lite_p2p::network iface(ifc);
+
+        ifaces_info.push_back(iface);
+        
+        addrs.push_back(iface.ip);
+        for (auto &&ip6 : iface.ip6) {
+            addrs.push_back(ip6);
+        }
+    }
+}
