38dc067948545129138eba5694b58dfc45d0a910
Anis CHALI <anis.chali1@outlook.com>
2024-11-29 05:22:55 -0500

    crypto: implement sha1 and ensure we free all the memory we use

--- lib/crypto.cpp
+++ lib/crypto.cpp
@@ -14,22 +14,51 @@ int crypto::sha256(uint8_t *buf, size_t len, uint8_t *out_hash) {
     
     ret = EVP_DigestInit_ex(ctx, EVP_sha256(), NULL);
     if (!ret)
-        return -errno;
+        goto out_err;
 
     ret = EVP_DigestUpdate(ctx, buf, len);
     if (!ret)
-        return -errno;
+        goto out_err;
 
     ret = EVP_DigestFinal_ex(ctx, out_hash, &out_len);
     if (!ret)
-        return -errno;
+        goto out_err;
+
+    EVP_MD_CTX_free(ctx);
 
     return (int)out_len;
+
+out_err:
+    EVP_MD_CTX_free(ctx);
+    return -EINVAL;
 }
 
 
-int crypto::sha1(uint8_t *buf, size_t len, uint8_t *key) {
+int crypto::sha1(uint8_t *buf, size_t len, uint8_t *out_hash) {
+    EVP_MD_CTX *ctx = EVP_MD_CTX_new();
+    int ret;
+    uint32_t out_len = 0;
 
+    if (!ctx)
+        return -ENOMEM;
+    
+    ret = EVP_DigestInit_ex(ctx, EVP_sha1(), NULL);
+    if (!ret)
+        goto out_err;
 
-    return 0;
+    ret = EVP_DigestUpdate(ctx, buf, len);
+    if (!ret)
+        goto out_err;
+
+    ret = EVP_DigestFinal_ex(ctx, out_hash, &out_len);
+    if (!ret)
+        goto out_err;
+
+    EVP_MD_CTX_free(ctx);
+
+    return (int)out_len;
+
+out_err:
+    EVP_MD_CTX_free(ctx);
+    return -EINVAL;
 }
--- test/lite-crypto.cpp
+++ test/lite-crypto.cpp
@@ -3,19 +3,28 @@
 #include <cstring>
 #include "lite-p2p/crypto.hpp"
 
+void print_hexbuf(uint8_t *buf, int len) {
 
+    for (int i = 0; i < len; ++i) {
+        printf("%02x", buf[i]);
+    }
+
+    printf("\n");
+}
 
 
 int main(int argc, char *argv[]) {
     int ret;
     uint8_t hash[EVP_MAX_MD_SIZE];
 
+    memset(hash, 0x0, EVP_MAX_MD_SIZE);
     ret = lite_p2p::crypto::sha256((uint8_t *)argv[1], strlen(argv[1]), hash);
+    print_hexbuf(hash, ret);
 
-    for (int i = 0; i < ret; ++i) {
-        printf("%02x", hash[i]);
-    }
 
-    printf("\n");
+    memset(hash, 0x0, EVP_MAX_MD_SIZE);
+    ret = lite_p2p::crypto::sha1((uint8_t *)argv[1], strlen(argv[1]), hash);
+    print_hexbuf(hash, ret);
+
     return 0;
 }
