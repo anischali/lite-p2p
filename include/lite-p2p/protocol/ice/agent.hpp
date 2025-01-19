5ceb4c4ea66246df764040982c015a78f837322d
Anis CHALI <anis.chali1@outlook.com>
2025-01-18 23:18:26 -0500

    agent: implement ice candidates (WIP).

--- include/lite-p2p/protocol/ice/agent.hpp
+++ include/lite-p2p/protocol/ice/agent.hpp
@@ -2,22 +2,25 @@
 #define __ICE_AGENT_HPP__
 #include <vector>
 #include <lite-p2p/network/network.hpp>
-
+#include <lite-p2p/network/socket.hpp>
 namespace lite_p2p::protocol::ice
 {
 
     class agent
     {
     private:
+        base_socket *sock;
         std::vector<struct sockaddr_t> addrs;
         std::vector<lite_p2p::network> ifaces_info;
 
         /* data */
     public:
-        agent();
+        agent(base_socket *s);
         ~agent();
 
         void gather_addrs(void);
+        void gather_candidates(std::vector<struct stun_session_t> s_stuns);
+        void send_candidates(struct sockaddr_t *remote);
         std::vector<struct sockaddr_t> get_addrs(void) {return addrs;};
     };
 };
--- lib/protocol/ice/agent.cpp
+++ lib/protocol/ice/agent.cpp
@@ -1,9 +1,17 @@
 #include <set>
-#include "lite-p2p/protocol/ice/agent.hpp"
+#include <lite-p2p/protocol/ice/agent.hpp>
+#include <lite-p2p/network/socket.hpp>
 
 using namespace lite_p2p::protocol::ice;
 
-agent::agent()
+struct ice_candidate {
+    int type;
+    struct sockaddr_t bind_addr;
+    struct sockaddr_t addr;
+    struct stun_session_t *session;
+};
+
+agent::agent(base_socket *s) : sock{s}
 {
 }
 
@@ -26,4 +34,16 @@ void agent::gather_addrs(void) {
         
         ifaces_info.push_back(iface);
     }
+}
+
+void agent::gather_candidates(std::vector<struct stun_session_t> s_stuns) {
+
+    gather_addrs();
+
+    // foreach stun/turn and each interface gather addresses
+    // try direct first and then try turn sessions.
+}
+
+void agent::send_candidates(struct sockaddr_t *remote) {
+    
 }
